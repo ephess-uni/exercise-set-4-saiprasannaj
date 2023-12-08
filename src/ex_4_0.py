fc1a3e6d1bc287c06b819b92ede18ef86198d992
saiprasannaj <149258559+saiprasannaj@users.noreply.github.com>
2023-12-08 07:13:59 +0300

    Update ex_4_0.py
    Complete

--- src/ex_4_0.py
+++ src/ex_4_0.py
@@ -1,46 +1,18 @@
-
 """ ex_4_0.py """
 try:
     from src.util import get_data_file_path
 except ImportError:
     from util import get_data_file_path
 
-# Use this FILENAME variable to test your function.
 FILENAME = get_data_file_path('messages.log')
-# >>>> DO NOT MODIFY CODE ABOVE <<<<
-""" ex_4_0.py """
-try:
-    from src.util import get_data_file_path
-except ImportError:
-    from util import get_data_file_path
 
-# Use this FILENAME variable to test your function.
-FILENAME = get_data_file_path('messages.log')
-# >>>> DO NOT MODIFY CODE ABOVE <<<
 def get_shutdown_events(logfile):
-    shutdown_entries = []
-
-    with open(logfile, 'r') as file:
-        for line in file:
+    shutdown_lines = []
+    with open(logfile, "r") as f:
+        for line in f:
             if "Shutdown initiated" in line:
-                shutdown_entries.append(line.strip())
+                shutdown_lines.append(line.strip())
+    return shutdown_lines
 
-    return shutdown_entries
-
-# Example usage:
 if __name__ == "__main__":
-    try:
-        from src.util import get_data_file_path
-    except ImportError:
-        from util import get_data_file_path
-
-    # Use this FILENAME variable to test your function.
-    FILENAME = get_data_file_path('messages.log')
-
-    shutdown_events = get_shutdown_events(FILENAME)
-
-    if shutdown_events:
-        for event in shutdown_events:
-            print(event)
-    else:
-        print("No shutdown events found.")
+    print(f"{get_shutdown_events(FILENAME)=}")
